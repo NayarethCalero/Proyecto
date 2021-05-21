f9a1a1aed11b6728d3dee68505d054fb77918b8f
Your Name <you@example.com>
2021-05-21 21:21:10 +0000

    creacion de las carpetas

---
+++ application.py
@@ -0,0 +1,42 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session,url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+
+
+app = Flask(__name__)
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+db = SQL("sqlite:///gorky.db")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/juego")
+@login_required
+def juego():
+    return render_template("juego.html")
+
+@app.route("/puntuaciones")
+def puntuaciones():
+    return render_template("puntuaciones.html")
